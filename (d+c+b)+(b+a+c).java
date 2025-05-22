982fccb3786d87f2d94b90bb7404c7c63021ef47
aravind777vardhan <aravind777.vardhan@gmail.com>
2025-05-22 17:07:57 +0530

    Add files via upload

---
+++ (d+c+b)+(b+a+c).java
@@ -0,0 +1,11 @@
+class d_c_b_b_a_c 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		char b='A';
+		boolean c=true;
+		String d="Happy";
+		System.out.println((d+c+b)+(b+a+c));
+	}
+}
