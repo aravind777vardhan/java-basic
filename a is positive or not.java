5a4d4caf5abb8b023be4a93e40f67504bc7f63af
aravind777vardhan <aravind777.vardhan@gmail.com>
2025-05-23 16:56:12 +0530

    Add files via upload

---
+++ a is positive or not.java
@@ -0,0 +1,10 @@
+//-4,-3,-2,-1,0,1,2,3,4
+class a is positive or not 
+{
+	public static void main(String[] args) 
+	{
+		int a=11;
+		boolean result=(a>0);
+		System.out.println("The value"+a+"is positive:"+result);
+	}
+}
