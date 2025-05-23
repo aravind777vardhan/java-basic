8306bc9e75ccfb4b40ff6730aee9c038dbde7bc4
aravind777vardhan <aravind777.vardhan@gmail.com>
2025-05-23 16:56:40 +0530

    Add files via upload

---
+++ A less than B or not.java
@@ -0,0 +1,10 @@
+class A less than B or not 
+{
+	public static void main(String[] args) 
+	{
+		int a=2;
+		int b=4;
+		boolean result=(a<b);
+		System.out.println("The value"+a+"is less than"+b+"is :"+result);
+	}
+}
